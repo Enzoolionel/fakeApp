3d3d1770f2ab1d93439fc90296eac81c128bafcd
enzoolionel <enzoperaltalio@gmail.com>
2024-11-25 11:34:24 -0300

    Solucionanando estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        cardMountBackground:
+          "linear-gradient(90deg, hsla(0, 80%, 94%, 1) 0%, hsla(0, 0%, 85%, 1) 100%)",
+
+        cardBackground:
+          "linear-gradient(45deg, hsla(0, 80%, 99%, 1) 0%, hsla(0, 0%, 95%, 1) 100%)",
+      },
+    },
   },
   plugins: [],
 };
